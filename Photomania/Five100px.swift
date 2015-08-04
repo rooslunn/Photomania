a5bc0b9187a16331a831f8623729569b02fb2068
Russ <russ@MacBook-Air.local>
2015-08-04 20:08:53 +0300

    Creating a Request Router

--- Photomania/Five100px.swift
+++ Photomania/Five100px.swift
@@ -7,15 +7,46 @@
 //
 
 import UIKit
+import Alamofire
 
 struct Five100px {
-  enum ImageSize: Int {
-    case Tiny = 1
-    case Small = 2
-    case Medium = 3
-    case Large = 4
-    case XLarge = 5
-  }
+    enum Router: URLRequestConvertible {
+        static let baseURLString = "https://api.500px.com/v1"
+        static let consumerKey = "wxsjtW0CL0T3eEkLgAH4FoW6S3VrbYH8yrxPvsey"
+        
+        case PopularPhotos(Int)
+        case PhotoInfo(Int, ImageSize)
+        case Comments(Int, Int)
+        
+        var URLRequest: NSURLRequest {
+            let (path: String, parameters: [String: AnyObject]) = {
+                switch self {
+                case .PopularPhotos (let page):
+                    let params = ["consumer_key": Router.consumerKey, "page": "\(page)", "feature": "popular", "rpp": "50",  "include_store": "store_download", "include_states": "votes"]
+                    return ("/photos", params)
+                case .PhotoInfo(let photoID, let imageSize):
+                    var params = ["consumer_key": Router.consumerKey, "image_size": "\(imageSize.rawValue)"]
+                    return ("/photos/\(photoID)", params)
+                case .Comments(let photoID, let commentsPage):
+                    var params = ["consumer_key": Router.consumerKey, "comments": "1", "comments_page": "\(commentsPage)"]
+                    return ("/photos/\(photoID)/comments", params)
+                }
+            }()
+            
+            let URL = NSURL(string: Router.baseURLString)
+            let URLRequest = NSURLRequest(URL: URL!.URLByAppendingPathComponent(path))
+            let encoding = Alamofire.ParameterEncoding.URL
+            
+            return encoding.encode(URLRequest, parameters: parameters).0
+        }
+    }
+    enum ImageSize: Int {
+        case Tiny = 1
+        case Small = 2
+        case Medium = 3
+        case Large = 4
+        case XLarge = 5
+    }
 }
 
 class PhotoInfo: NSObject {
